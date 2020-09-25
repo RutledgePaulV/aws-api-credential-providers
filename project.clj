210979f2c6b15822cf6a91002973f7c7ca0a4bd8
Paul Rutledge <paul.rutledge@rate.com>
2020-09-24 20:45:45 -0500

    fixes

--- project.clj
+++ project.clj
@@ -1,5 +1,29 @@
 (defproject org.clojars.rutledgepaulv/aws-api-credential-providers "0.1.0-SNAPSHOT"
 
+  :description
+  "A library providing more featureful implementations of aws authentication providers for cognitect's aws-api library."
+
+  :url
+  "https://github.com/rutledgepaulv/aws-api-credential-providers"
+
+  :license
+  {:name "MIT License" :url "http://opensource.org/licenses/MIT" :year 2020 :key "mit"}
+
+  :scm
+  {:name "git" :url "https://github.com/rutledgepaulv/aws-api-credential-providers"}
+
+  :pom-addition
+  [:developers
+   [:developer
+    [:name "Paul Rutledge"]
+    [:url "https://github.com/rutledgepaulv"]
+    [:email "rutledgepaulv@gmail.com"]
+    [:timezone "-5"]]]
+
+  :deploy-repositories
+  [["releases" :clojars]
+   ["snapshots" :clojars]]
+
   :dependencies
   [[org.clojure/clojure "1.10.1"]
    [com.cognitect.aws/api "0.8.474"]
